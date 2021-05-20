4045c8c90b6b0765bdf2188def9c792088f2c03a
Girma Nigusse <xgirma@gmail.com>
2021-05-20 02:16:19 -0400

    publish

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
-const builder = require('./data/builder.json');
-const decorator = require('./data/decorator.json');
-const facade = require('./data/facade.json');
-const factory = require('./data/factory.json');
-const strategy = require('./data/strategy.json');
-
+const builder = require("./data/builder.json");
+const decorator = require("./data/decorator.json");
+const facade = require("./data/facade.json");
+const factory = require("./data/factory.json");
+const strategy = require("./data/strategy.json");
 
 module.exports = {
-    builder, decorator, facade, factory, strategy
-}
+  builder,
+  decorator,
+  facade,
+  factory,
+  strategy,
+};
